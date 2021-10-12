fafb4bbc935f7c88bfd4b3fd3d2644bf3ac867ac
Dan Huynh <danhuynh99@gmail.com>
2021-10-12 16:05:53 -0500

    First major push, logic for calculating points is initially done, need to test more scenarios

---
+++ TinyScan/Model/Square.swift
@@ -0,0 +1,19 @@
+//
+//  Square.swift
+//  TinyScan
+//
+//  Created by Dan Huynh on 12/29/20.
+//  Copyright © 2020 Dan Huynh. All rights reserved.
+//
+
+import Foundation
+
+public class Square {
+        
+    let type : String
+    
+    public init(name: String){
+        type = name
+    }
+}
+
---
+++ TinyScan/Model/TheaterDict.swift
@@ -0,0 +1,18 @@
+//
+//  TheaterDict.swift
+//  TinyScan
+//
+//  Created by Dan Huynh on 9/29/21.
+//  Copyright © 2021 Dan Huynh. All rights reserved.
+//
+
+import Foundation
+
+//Dictionary created to calculate how many points a theater will generate
+public class TheaterDict {
+    var list: Dictionary<String,Bool>
+    
+    public init(){
+        list = [String: Bool]()
+    }
+}
